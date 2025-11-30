94f5cae5b9d78274de3fa6e0c57d521f4aac4c35
MoschinoMoscato <moscaleonardo2007@gmail.com>
2025-11-30 18:26:46 +0000

    tutorial

---
+++ tutorial/variables_case_sensitivity.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$color = "red";
+echo "My car is " . $color;
+echo "My house is " . $color . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?>
+
+</body>
+</html>
